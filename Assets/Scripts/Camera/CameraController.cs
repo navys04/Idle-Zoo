8cdbc3da81b8b3334d0f24422476c3fca7c91ac0
Sarmat <navyss26@outlook.com>
2023-06-27 12:36:02 +0300

    Scroll

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private bool _isPressed = false;
+    private float _vertical;
+
+    [SerializeField] private Vector2 edges;
+    [SerializeField] private float scrollSpeed = 1.0f;
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _isPressed = true;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            _isPressed = false;
+        }
+        
+        _vertical = Input.GetAxis("Mouse Y");
+        
+        if (_isPressed) Scroll();
+    }
+
+    private void Scroll()
+    {
+        float deltaYPos = scrollSpeed * _vertical * -1;
+        float newYPos = Mathf.Clamp(transform.position.y + deltaYPos, edges.x, edges.y);
+        
+        
+        transform.position = new Vector3(transform.position.x,
+            newYPos, transform.position.z);
+        
+        print(_vertical);
+    }
+}
